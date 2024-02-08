58c53dfc5195c593bf3dfb36fc76905141f517a2
Nikunj Mehra <nikunjmehra935@gmail.com>
2024-02-08 16:02:37 +0530

    Complete landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Figtree } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="light">
+      <body
+        className={cn(
+          "min-h-screen font-sans antialiased grainy",
+          font.className
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/MaxWidthWrapper.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+
+interface MaxWidthWrapperProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+const MaxWidthWrapper: React.FC<MaxWidthWrapperProps> = ({
+  className,
+  children,
+}) => {
+  return (
+    <div
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
